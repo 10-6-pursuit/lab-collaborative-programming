58a3b6a38c5caaf8269b76be0b0949fe2829ce3c
MarcusB86 <marcusb08@gmail.com>
2024-06-05 14:51:58 -0400

    labComplete

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(words) {
+  return words.sort((a, b) => a.length - b.length)
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +16,47 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(str) {
+      rotatingStr = [];
+      
+      for (let i = 1; i <= str.length; i++) {
+        
+        const rotateStr = str.slice(i) + str.slice(0, i);
+        
+        rotatingStr.push(rotateStr);
+      }
+      return rotatingStr;
+
+
+
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(numArr) {
+  
+  if (numArr.length < 1 ) {
+    return [];
+  } else if (!numArr.every(element =>  typeof element === 'number')) {
+    return numArr.slice();
+  }
+    
+  
+  
+  let largest = Math.max(...numArr);
+  let smallest = Math.min(...numArr);
+  
+  return largest - smallest;
+  
+}
+  
+
+
+
+
 
 /**
  * Returns the message translated into morse code
@@ -31,7 +66,21 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+  let morseMessage = [];
+
+  for (let i = 0; i < message.length; i++) {
+     
+    let char = message[i].toUpperCase();
+
+    if ( char in dictionary) {
+      morseMessage.push(dictionary[char]);
+    } 
+  }
+  return morseMessage.join(' ');
+
+
+}
 
 module.exports = {
   sortByStringLength,
