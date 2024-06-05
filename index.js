c804e2bce8c7e87fcc55fe61dd927a68d42edcfe
ayoublos <abelibel@pursuit.org>
2024-06-04 20:10:01 -0700

    finish the lab

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
-
+function sortByStringLength(array) {
+return array.sort((a,b)=>a.length-b.length)
+}
 /**
  * Returns an array of the word in all scrolling positions.
  * @param {String} word - A string.
@@ -14,15 +15,42 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+  let newWord=word.split(``)
+  let array=[]
+
+for(let i=0;i<word.length;i++){
+    newWord.push(newWord.shift())
+    array.push(newWord.join(``))
+    
+
+
+}
+
+    return array
+}
+
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(array) {
+ 
+  for(let x of array){
+    if(typeof x !==`number`){
+      return array
+    }
+  }
+//  if( array.some(el=>typeof el!==`number`)){
+//   return array
+//  }
+array.sort((a,b)=>b-a)
+// return Math.max(...array)-Math.min(...array)
 
+return array[0]-array[array.length-1]
+}
 /**
  * Returns the message translated into morse code
  * @param {String} message - A string to translate.
@@ -31,7 +59,17 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message) {
+  let array=[]
+  for(let x of message){
+  
+   if(x!==` `){
+      array.push(morseCodeDictionary[x.toUpperCase()])
+    }
+
+  }
+  return array.join(` `)
+}
 
 module.exports = {
   sortByStringLength,
